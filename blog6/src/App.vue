36b8009dd4121579737434e0204b645cce65d9f5
Signeemmanuel <signeemmanuel28@gmail.com>
2023-04-15 06:29:23 +0100

    Add blog6

---
+++ blog6/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h1>Routing</h1>
+  <router-link to="/">Home</router-link>
+  <br>
+  <router-link to='/login'>Login</router-link>
+  <br>
+  <router-link to='/profile/john'>John's Profile</router-link>
+  <br>
+  <router-link to='/profile/josue'>Josue's profile</router-link>
+  <br>
+  <router-link to='/profile/mary'>Maries's profile</router-link>
+  <router-view/>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+
+</style>
---
+++ blog6/src/components/Profile.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>{{profile}}'s Profile</h1>
+</template>
+<script>
+    import {useRoute} from 'vue-router'
+
+    export default {
+        name: "ProfileComponent",
+        data() {
+            return {
+                profile: "",
+            }
+        },
+
+        mounted() {
+            let route = useRoute();
+            
+            this.profile = route.params.name
+            console.log(this.profile)
+        },
+    }
+</script>
+<style>
+</style>
